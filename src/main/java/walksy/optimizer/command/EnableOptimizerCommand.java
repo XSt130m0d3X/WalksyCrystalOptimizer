2178d3c6de694ad4d8d1eb5931cc2907d6cad297
XSt130m0d3X <89541133+XSt130m0d3X@users.noreply.github.com>
2024-07-04 19:45:30 +0100

    EnableOptimizerCommand.java

--- src/main/java/walksy/optimizer/command/EnableOptimizerCommand.java
+++ src/main/java/walksy/optimizer/command/EnableOptimizerCommand.java
@@ -14,13 +14,13 @@ public class EnableOptimizerCommand {
 
     public void initializeToggleCommands() {
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
-                dispatcher.register(ClientCommandManager.literal("walksyfastcrystal").executes(context -> {
+                dispatcher.register(ClientCommandManager.literal("hydrogen").executes(context -> {
                     if (fastCrystal) {
                         fastCrystal = false;
-                        displayMessage("Walksy's Fast crystals disabled!");
+                        displayMessage("Hydrogen disabled!");
                     } else if (!fastCrystal) {
                         fastCrystal = true;
-                        displayMessage("Walksy's Fast crystals enabled");
+                        displayMessage("Hydrogen enabled");
                     }
                     return 1;
                 }))
